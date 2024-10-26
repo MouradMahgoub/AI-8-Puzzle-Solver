d5d3411df0b8cc187de1db2da220769062e240f9
Aly El Din <eng.alyeldin@gmail.com>
2024-10-26 05:40:31 +0300

    Results handled for BFS and well-tested

--- src/strageties/bfs_solver.py
+++ src/strageties/bfs_solver.py
@@ -8,12 +8,14 @@ class BFSSolver(SolverStrategy):
     def solve(self):
         frontier = [self.puzzle]  
         visited = set()
+        parents = {self.puzzle.state: None}  
 
         while frontier:
             current_puzzle = frontier.pop(0)  
-
+            self.nodes_expanded += 1            
+            
             if current_puzzle.is_goal(): 
-                return True
+                return self.get_path(parents, current_puzzle.state)
 
             visited.add(current_puzzle.state)  
 
@@ -21,7 +23,15 @@ class BFSSolver(SolverStrategy):
 
             for child_state in child_states:
                 child_puzzle = Puzzle(current_puzzle.int_to_state(child_state))
-
                 if child_puzzle.state not in visited:
                     frontier.append(child_puzzle)  
-        return False
+                    parents[child_puzzle.state] = current_puzzle.state
+        return None
+    
+    def path_results(self, path):
+        self.max_depth = len(path) - 1
+        return self.get_result(path[-1])
+
+
+    
+        
--- src/strageties/solver_strategy.py
+++ src/strageties/solver_strategy.py
@@ -1,6 +1,6 @@
 from abc  import ABC, abstractmethod
-from result import Result
 import time
+from result import Result
 
 class SolverStrategy(ABC):
     @abstractmethod
@@ -16,6 +16,7 @@ class SolverStrategy(ABC):
         self.explored = set()
         self.parent_map = {}
         self.start_time = None
+        self.path = []
         
     def start_timer(self):
         self.start_time = time.time()
@@ -23,15 +24,20 @@ class SolverStrategy(ABC):
     def stop_timer(self):
         return time.time() - self.start_time
     
-    def get_path(self, goal_state):
-        # Get the path from the goal state to the start state
-        pass
+    def get_path(self, parents, goal_state):
+        # Get the path from the start state to the goal state
+        path = []
+        state = goal_state
+        while state is not None:
+            path.append(state)
+            state = parents[state]
+        path.reverse()  
+        self.path = path
+        return path
     
     def get_result(self, goal_state):
         if goal_state is None:
             return Result(None, 0, self.nodes_expanded, self.max_depth, self.stop_timer())
         
-        path = self.get_path(goal_state)
-        
-        return Result(path, len(path), self.nodes_expanded, self.max_depth, self.stop_timer())
+        return Result(self.path, len(self.path), self.nodes_expanded, self.max_depth, self.stop_timer())
         pass
