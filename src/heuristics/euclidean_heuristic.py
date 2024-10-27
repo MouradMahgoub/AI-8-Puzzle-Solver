0086be2bb38a8ee04c74a251c1beea28900b9993
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2024-10-27 22:09:13 +0300

    A* Implementation (#13)

--- src/heuristics/euclidean_heuristic.py
+++ src/heuristics/euclidean_heuristic.py
@@ -1,6 +1,20 @@
-from heuristics import HeuristicStrategy
-    
-class EuclideanHeuristic(HeuristicStrategy):
-    def calculate(self, puzzle):
-        # Implement Euclidean heuristic here
-        pass
+import math
+
+
+class EuclideanHeuristic:
+    def calculate(self, state):
+        state_str = str(state)
+        if len(state_str) == 8:
+            state_str = "0" + state_str
+
+        goal_state = "012345678"
+        h = 0
+
+        for i in range(9):
+            if state_str[i] != '0':
+                goal_position = goal_state.index(state_str[i])
+                vertical_difference = abs(i // 3 - goal_position // 3)
+                horizontal_difference = abs(i % 3 - goal_position % 3)
+                h += math.sqrt(vertical_difference ** 2 + horizontal_difference ** 2)
+
+        return h
